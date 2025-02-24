545b19a821048705e8c310a7007b3037f3fd3382
Helder de Lima Dantas Neto <neto0309@hotmail.com>
2025-02-24 19:15:55 -0300

    Added selectors list for login spec

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,18 +1,27 @@
 describe('Orange HRM Test', () => {
+
+  const selectorList = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+    wrongCredentialAlert: "[role='alert']"
+  }
+
   it('Login - Success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('Admin')
-    cy.get("[name='password']").type('admin123')
-    cy.get("[type='submit']").click()
+    cy.get(selectorList.usernameField).type('Admin')
+    cy.get(selectorList.passwordField).type('admin123')
+    cy.get(selectorList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(".oxd-topbar-header-breadcrumb-module").contains('Dashboard') //verefica se existe
+    cy.get(selectorList.sectionTitleTopBar).contains('Dashboard') //verefica se existe
   })
 
   it('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('Test')
-    cy.get("[name='password']").type('test')
-    cy.get("[type='submit']").click()
-    cy.get("[role='alert']") //não focar nos textos mas sim nas box
+    cy.get(selectorList.usernameField).type('Test')
+    cy.get(selectorList.passwordField).type('test')
+    cy.get(selectorList.loginButton).click()
+    cy.get(selectorList.wrongCredentialAlert) //não focar nos textos mas sim nas box
   })
 })
